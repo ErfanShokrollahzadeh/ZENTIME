2222b19c8b9e1005d1c741f43e0efd86b59e2b81
RenderKar <Amirmahdizadh143@gmail.com>
2025-08-04 22:30:48 +0330

    the  first edit

--- server/landtime/admin.py
+++ server/landtime/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'is_exist', 'rating')
+    list_filter = ('is_exist',)
+    search_fields = ('title', 'short_description')
+    ordering = ('-price',)
+    list_editable = ('price', 'is_exist')
+
+    def __str__(self):
+        return self.title
+
+
+#username = admin
+#password = admin1234@
--- server/landtime/models.py
+++ server/landtime/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    short_description = models.TextField(max_length=500, blank=True)
+    is_exist = models.BooleanField(default=True)
+    rating = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.title
